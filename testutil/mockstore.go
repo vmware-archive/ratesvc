f0e4921f27463b1ab2c776fc959870a58edef76c
Andres Martinez Gotor <andres@bitnami.com>
2020-02-04 13:35:40 +0100

    Fix test

--- testutil/mockstore.go
+++ testutil/mockstore.go
@@ -1,9 +1,9 @@
 package testutil
 
 import (
+	"github.com/globalsign/mgo"
 	"github.com/kubeapps/common/datastore"
 	"github.com/stretchr/testify/mock"
-	mgo "gopkg.in/mgo.v2"
 )
 
 // MockSession acts as a mock datastore.Session
@@ -14,6 +14,10 @@ type mockSession struct {
 // DB returns a mocked datastore.Database and empty closer function
 func (s mockSession) DB() (datastore.Database, func()) {
 	return mockDatabase{s.Mock}, func() {}
+}
+
+func (s mockSession) Use(name string) datastore.Session {
+	return s
 }
 
 // mockDatabase acts as a mock datastore.Database
@@ -28,6 +32,10 @@ func (d mockDatabase) C(name string) datastore.Collection {
 // mockCollection acts as a mock datastore.Collection
 type mockCollection struct {
 	*mock.Mock
+}
+
+func (c mockCollection) Bulk() datastore.Bulk {
+	return mockBulk{c.Mock}
 }
 
 func (c mockCollection) Find(query interface{}) datastore.Query {
@@ -48,11 +56,15 @@ func (c mockCollection) Upsert(selector interface{}, update interface{}) (*mgo.C
 }
 
 func (c mockCollection) UpsertId(selector interface{}, update interface{}) (*mgo.ChangeInfo, error) {
+	c.Called(selector, update)
 	return nil, nil
 }
 
 func (c mockCollection) UpdateId(selector, update interface{}) error {
-	c.Called(selector, update)
+	args := c.Called(selector, update)
+	if len(args) > 0 {
+		return args.Error(0)
+	}
 	return nil
 }
 
@@ -60,8 +72,34 @@ func (c mockCollection) Remove(selector interface{}) error {
 	return nil
 }
 
+func (c mockCollection) RemoveAll(selector interface{}) (*mgo.ChangeInfo, error) {
+	c.Called(selector)
+	return nil, nil
+}
+
 func (c mockCollection) Count() (int, error) {
 	return 0, nil
+}
+
+func (c mockCollection) Pipe(pipeline interface{}) datastore.Pipe {
+	return mockPipe{c.Mock}
+}
+
+// mockBulk acts as a mock datastore.Bulk
+type mockBulk struct {
+	*mock.Mock
+}
+
+func (b mockBulk) Upsert(pairs ...interface{}) {
+	b.Called(pairs)
+}
+
+func (b mockBulk) RemoveAll(selectors ...interface{}) {
+	b.Called(selectors)
+}
+
+func (b mockBulk) Run() (*mgo.BulkResult, error) {
+	return nil, nil
 }
 
 // mockQuery acts as a mock datastore.Query
@@ -76,7 +114,33 @@ func (q mockQuery) All(result interface{}) error {
 
 func (q mockQuery) One(result interface{}) error {
 	args := q.Called(result)
-	return args.Error(0)
+	if len(args) > 0 {
+		return args.Error(0)
+	}
+	return nil
+}
+
+func (q mockQuery) Sort(fields ...string) datastore.Query {
+	return q
+}
+
+func (q mockQuery) Select(selector interface{}) datastore.Query {
+	return q
+}
+
+// mockPipe acts as a mock datastore.Pipe
+type mockPipe struct {
+	*mock.Mock
+}
+
+func (p mockPipe) All(result interface{}) error {
+	p.Called(result)
+	return nil
+}
+
+func (p mockPipe) One(result interface{}) error {
+	p.Called(result)
+	return nil
 }
 
 // NewMockSession returns a mocked Session
